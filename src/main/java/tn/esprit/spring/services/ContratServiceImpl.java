04a6152e34ac12fa23fa598f1007ac70cb3d3716
wiemrk <rkikwiem@gmail.com>
2022-05-19 14:31:23 +0100

    commit test and log4j

--- src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
@@ -2,6 +2,8 @@ package tn.esprit.spring.services;
 
 import java.util.List;
 
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,13 @@ import tn.esprit.spring.repository.ContratRepository;
 @Service
 public class ContratServiceImpl implements IContratService {
 
-
+	private static final Logger log = Logger.getLogger(ContratServiceImpl.class);
 	@Autowired
 	ContratRepository contratRepository;
 
 
 	public List<Contrat> getAllContrats() {
+		log.info("fetching all contracts from the database ...");
 		return (List<Contrat>) contratRepository.findAll();
 	}
 
